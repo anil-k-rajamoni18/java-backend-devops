dbbe273945f088dbce5a641b358716b8850aec10
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-07-01 06:53:36 +0530

    update

--- JAVA/Example.java
+++ JAVA/Example.java
@@ -1,35 +1,21 @@
-abstract class Animal {
-    protected String name; // Protected variable
-    abstract void makeSound();      // Abstract method
-    abstract void displayInfo(); // Another abstract method
-    void eat() {                      // Concrete method
-        System.out.println("This animal eats food.");
-    }
-}
+import java.io.File;
+import java.io.IOException;
 
-class Dog extends Animal {
-    public Dog() { // Constructor
-        System.out.println("Dog class constructor called.");
-    }
 
-    @Override
-    void makeSound() {                // Implementing abstract method
-        System.out.println("Woof! Woof!");
-    }
-
-    @Override
-    void displayInfo() {              // Implementing another abstract method
-        System.out.println("This is a dog. Name: " + name);   
-    }
-}
 
 public class Example  {
-    public static void main(String[] args)  { 
-        Animal myDog = new Dog(); // Creating an instance of Dog
-        myDog.makeSound();                  // Calling the abstract method
-        myDog.eat();                        // Calling the concrete method
-        myDog.displayInfo();                // Calling the other abstract method
-        
+
+    public static void main(String[] args) { 
+        Example obj = new Example();
+        System.out.println("Hashcode: " + obj.hashCode());
+        obj = null;
+        System.gc();
+        System.out.println("End of garbage collection");
+    }
+
+    @Override
+    protected void finalize() {
+        System.out.println("Finalize method called");
     }
 }
 
