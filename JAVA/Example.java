e47a62621f6ecd2b890bc6e040b7d9332bd643ae
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-07-16 06:37:43 +0530

    update

--- JAVA/Example.java
+++ JAVA/Example.java
@@ -1,8 +1,36 @@
 import java.util.*;
+import java.util.function.Function;
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return id + " - " + name;
+    }
+}
+
+
 
 public class Example  {
   public static void main(String[] args) {
-      List<> numbers = new ArrayList<>(Arrays.asList(11, 20, 3, 41, 5,3,20,41,3));
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(2, "Alice"));
+        students.add(new Student(1, "John"));
+        students.add(new Student(3, "Bob"));
+
+        
+        Collections.sort(students, (s1, s2) -> s1.name.compareTo(s2.name));  
+        System.out.println("Sorted by ID: " + students);
+
+        List<String> names = Arrays.asList("John", "Alice", "Bob");
+        names.forEach(name -> System.out.println("Name: " + name));
+
       
     }           
 }
