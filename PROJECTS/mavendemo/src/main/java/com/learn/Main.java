b2da931424472e1652a4a6cc41b9b84c4fbfb74e
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-08-13 06:42:43 +0530

    update

--- PROJECTS/mavendemo/src/main/java/com/learn/Main.java
+++ PROJECTS/mavendemo/src/main/java/com/learn/Main.java
@@ -1,7 +1,19 @@
 package com.learn;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Properties props = new Properties();
+        try (InputStream in = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(props.getProperty("message"));
+        System.out.println(props.getProperty("environment"));
+        System.out.println(props.getProperty("database.url"));
     }
 }
